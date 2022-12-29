459b82786579bab0c558ec4a3779a6d4f25f61b5
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2022-12-29 13:39:35 -0300

    Modification to VCInverters become a compossed class type

--- main.py
+++ main.py
@@ -2,23 +2,7 @@ import pandas as pd
 import os
 import vc_inverter as inv
 
-def get_data_dict( df ):
-    keys          = []
-    dict_sys_data = {}
-    vc_inv        = {}
-    basic_data    = {}
-    droop_data    = {}
-
-    i = 0
-    for column in df:
-        if i < 5:
-            basic_data[ column ] = [ df.iloc[0:len(df.index), i] ]
-        i += 1
-        
-    dict_sys_data[ 'vc_inverters' ] = basic_data
-    return dict_sys_data
-
-def read_data( DATA_FILE , sheet_to_read ):
+def read_data_from_sheet( DATA_FILE , sheet_to_read ):
     abs_path = os.path.dirname(__file__)
     rel_path = "Data"
     full_path = os.path.join(abs_path, rel_path)
@@ -33,16 +17,16 @@ def get_list_vc_invs( df_sys_data ):
 
 def main():
     DATA_FILE = 'Sample_data.xlsx'
-    sheet_to_read = 'VCInverter_data'
-    df_sys_data = read_data( DATA_FILE, sheet_to_read )
+    sheet_to_read = 'VCInverter_data' #depois fazer lista com os nomes das sheets e func para iterar
+    df_sys_data = read_data_from_sheet( DATA_FILE, sheet_to_read )
 
     print(df_sys_data)
     #print(df_sys_data['kq [V/Var]'] )
 
     l_vc_invs = get_list_vc_invs( df_sys_data )
-    print(l_vc_invs[0].Droop.get_ss_model())
-    print('o parametro droop é: ', l_vc_invs[0].Droop.w_fp)
-    print('o parametro droop é: ', l_vc_invs[1].Droop.w_fp)
+    print(l_vc_invs[0].droop.Ap)
+    print('o parametro droop é: ', l_vc_invs[0].droop.w_fp)
+    print('o parametro droop é: ', l_vc_invs[1].droop.w_fp)
     print(len(l_vc_invs))
 
 
--- vc_inverter.py
+++ vc_inverter.py
@@ -1,28 +1,31 @@
 import numpy as np
+import pandas as pd
 
 class VCInverter:
     
     def __init__( self, df_data, w_nom=376.999 ):
         self.w_nom = w_nom
-        self.Droop = self.Droop(df_data)
+        self.droop = Droop(df_data)
 
-    class Droop:
-        def __init__(self, df_data):
-            self.kp   = df_data.iloc[5]
-            self.kq   = df_data.iloc[6]
-            self.w_fp = df_data.iloc[7]
-            self.w_fq = df_data.iloc[8]
-            self.Rv   = df_data.iloc[9]
-            self.Lv   = df_data.iloc[10]
-            
-            self.Ap   = np.array([])
+
+class Droop: #criar inner class basic data, etc
+    def __init__(self, df_data):
+        self.kp   = df_data.iloc[5]
+        self.kq   = df_data.iloc[6]
+        self.w_fp = df_data.iloc[7]
+        self.w_fq = df_data.iloc[8]
+        self.Rv   = df_data.iloc[9]
+        self.Lv   = df_data.iloc[10]
+
+        self.Ap   = self.get_ss_model()    
+        #self.Ap   = np.array([])
         
-        def get_ss_model( self ):
-            self.Ap = np.array( [ [ 0 , -self.kp   , 0 ],
-                                [ 0 , -self.w_fp , 0 ],
-                                [ 0 ,  0         , -self.w_fq] ] )
+    def get_ss_model( self ):
+        self.Ap = np.array( [ [ 0 , -self.kp   , 0 ],
+                              [ 0 , -self.w_fp , 0 ],
+                              [ 0 ,  0         , -self.w_fq] ] )
             
-            return self.Ap
+        return self.Ap
 
     
 
